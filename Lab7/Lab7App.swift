a8d9a110d585d03d5a6d09d76e2731aadab059b6
Eric Smith <ics21-007@camosun.bc.ca>
2023-02-28 13:25:28 -0800

    Initial Commit

---
+++ Lab7/Lab7App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab7App.swift
+//  Lab7
+//
+//  Created by IMD 224 on 2023-02-28.
+//
+
+import SwiftUI
+
+@main
+struct Lab7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
